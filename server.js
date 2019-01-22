dd3653e5cbb149a4f51bf079aa0ea127f6e26378
matheuslemke <andersonmatheus.lemke@gmail.com>
2019-01-21 23:29:46 -0200

    Changing app name

--- server.js
+++ server.js
@@ -3,11 +3,11 @@ const path = require('path');
 
 const app = express();
 
-app.use(express.static(__dirname + '/dist/tasklist-supero-client'));
+app.use(express.static(__dirname + '/dist/tasklistClient'));
 
 app.get('/*', function(req,res) {
     
-res.sendFile(path.join(__dirname+'/dist/tasklist-supero-client/index.html'));
+res.sendFile(path.join(__dirname+'/dist/tasklistClient/index.html'));
 });
 
 app.listen(process.env.PORT || 8080);
